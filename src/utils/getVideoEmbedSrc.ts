17243512de8afba8d57970da48f9463beb730cd2
NguyenTam2206 <55978864+NguyenTam2206@users.noreply.github.com>
2023-03-29 21:15:13 +0700

    fix bug regex safari

--- src/utils/getVideoEmbedSrc.ts
+++ src/utils/getVideoEmbedSrc.ts
@@ -1,5 +1,6 @@
 export function getVideoEmbedSrc(url: string) {
-  const regex = /(?<=v=)[\w-]+/; // positive lookbehind to match "v=" followed by video ID
-  const match = url.match(regex); // try to find a match in the given URL
-  return match ? `https://www.youtube.com/embed/${match[0]}` : ""; // if a match is found, return the first capture group (video ID)
+  const youtubeRegex =
+    /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([\w-]+)(\S+)?$/;
+  const match = url.match(youtubeRegex);
+  return match ? `https://www.youtube.com/embed/${match[4]}` : null; // return the video ID capture group
 }
