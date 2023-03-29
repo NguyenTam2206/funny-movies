fd8628021c801f054a758c7e24a617c56636ed28
NguyenTam2206 <55978864+NguyenTam2206@users.noreply.github.com>
2023-03-29 21:17:00 +0700

    fix bug typescript

--- src/utils/getVideoEmbedSrc.ts
+++ src/utils/getVideoEmbedSrc.ts
@@ -2,5 +2,5 @@ export function getVideoEmbedSrc(url: string) {
   const youtubeRegex =
     /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([\w-]+)(\S+)?$/;
   const match = url.match(youtubeRegex);
-  return match ? `https://www.youtube.com/embed/${match[4]}` : null; // return the video ID capture group
+  return match ? `https://www.youtube.com/embed/${match[4]}` : ""; // return the video ID capture group
 }
