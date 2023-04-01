d00051b2e101f6e4cfef8f9c12be9ad84fafe70d
NguyenTam2206 <55978864+NguyenTam2206@users.noreply.github.com>
2023-04-01 08:18:24 +0700

    redirect if user is not logged in

--- src/pages/share.tsx
+++ src/pages/share.tsx
@@ -1,8 +1,18 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import ShareSection from "../components/ShareSection";
 import CommonLayout from "../layouts/Common";
+import { useRouter } from "next/router";
 
 function SharePage() {
+  const router = useRouter();
+  useEffect(() => {
+    const isLogedIn =
+      localStorage.getItem("user") && localStorage.getItem("token");
+    if (!isLogedIn) {
+      router.push("/");
+    }
+  }, [router]);
+
   return (
     <div className="mt-16">
       <div className="m-auto w-max">
